27193b26e9be0835428eed71a2d9d40f8437057e
Milos Popovic <155644532+milosp-89@users.noreply.github.com>
2024-01-10 10:25:22 +0100

    Create remove_duplicates.sql

---
+++ remove_duplicates.sql
@@ -0,0 +1,22 @@
+---- template script for removing duplicates ----
+
+declare @db_name varchar(50) = ' DeviceMagic_xxx' -- database name
+declare @tbl_name varchar(50) = 'xxx' -- table name
+
+select 'use' + @db_name + ';
+go
+
+WITH cte AS (
+	SELECT
+		submission_id,
+		ROW_NUMBER() OVER (
+			PARTITION BY 
+				submission_id
+			ORDER BY 
+				submission_id
+		) as row_num
+		FROM ' + @tbl_name + '
+)
+DELETE FROM cte
+WHERE row_num > 1;
+go'
